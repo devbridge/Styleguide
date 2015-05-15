557a93be96a6ed90d324a46806461e2d80192731
Mantas Miezinas <mantas.miezinas@devbridge.com>
2015-05-15 17:17:56 +0300

    Added initial snippets structure in sample.json.

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var config = require('../config.json');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'categories' });
+  res.json(config.categories);
 });
 
 module.exports = router;
--- routes/snippets.js
+++ routes/snippets.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var config = require('../config.json');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function (req, res) {
+router.get('/*.html', function (req, res) {
     res.sendFile('/snippets' + req.path, {'root': './'});
+});
+
+router.get('/:id', function (req, res) {
+  var dbPath = '../db/';
+  var result = require('../db/sample.json');
+  console.log(result);
+  res.json(result.sample[0]);
 });
 
 module.exports = router;
