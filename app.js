e4519d03299b4ea2f8a5472ee65d3884aac85d56
miezis <miezinas.mantas@gmail.com>
2015-06-17 15:56:11 +0300

    Cleaning up.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -55,7 +55,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
