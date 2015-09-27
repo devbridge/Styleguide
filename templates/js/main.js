540e523828e584a4440b2f9195034a619cac7e14
miezis <miezinas.mantas@gmail.com>
2015-09-27 18:45:09 +0300

    Temporary fix for snippet size indicator.

--- templates/js/main.js
+++ templates/js/main.js
@@ -111,7 +111,7 @@
                         if (e.rect.width > 150) {
                             iframe.style.width = e.rect.width + 'px';
                             target.style.width = e.rect.width + 'px';
-                            $sizeIndicator.val(e.rect.width + "px");
+                            $sizeIndicator.text(e.rect.width + "px");
                         }
                     },
                     onend: function (e) {
