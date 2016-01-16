907923ef636c3e6ed3ee16e9a8ae09093b428005
miezis <miezinas.mantas@gmail.com>
2016-01-16 19:50:06 +0200

    Added endpoint for category creation through API.

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,8 @@
-var express = require('express'),
+var _ = require('lodash'),
+	express = require('express'),
 	fs = require('fs'),
 	path = require('path'),
+	jf = require('jsonfile'),
 	dbConfigPath = path.join('./', 'styleguide', 'database_config.txt'),
 	databaseConfig = JSON.parse(fs.readFileSync(dbConfigPath, 'utf8')),
 	router = express.Router();
@@ -9,6 +11,32 @@ router.get('/', function(req, res) {
 	var categories = JSON.parse(fs.readFileSync(databaseConfig.categories, 'utf8'));
 
 	res.json(categories);
+});
+
+router.post('/', function (req, res) {
+	var categories = JSON.parse(fs.readFileSync(databaseConfig.categories, 'utf8')),
+		uniqueIds = _.pluck(categories, 'id'),
+		newCategory = {},
+		id = 1;
+
+	uniqueIds.sort(function (a, b) {
+		return a - b;
+	});
+
+	if (uniqueIds.length) {
+		id = uniqueIds[uniqueIds.length - 1 ] + 1;
+	}
+
+	newCategory = {
+		id: id,
+		name: req.body.name
+	};
+
+	categories.push(newCategory);
+
+	jf.writeFileSync(databaseConfig.categories, categories);
+
+	res.json(newCategory);
 });
 
 module.exports = router;
