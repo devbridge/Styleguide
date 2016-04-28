c99f56949be7b66fc60dbbb8cfefb4c0db4b0337
Vaclovas <vaclovas@devbridge.com>
2016-04-28 10:38:51 +0300

    #132 - added colored messages in console (success and failure mssages) #129 - port is being checked with tcpPort

--- README.md
+++ README.md
@@ -52,11 +52,13 @@ Setup grunt task:
 
 ```
 module.exports = function (grunt) {
-    var styleguide = require('devbridge-styleguide');
-    grunt.registerTask('start-styleguide', function () {
-        var done = this.async();
-        styleguide.startServer().on('close', done);
-    });
+	var styleguide = require('devbridge-styleguide');
+	grunt.registerTask('start-styleguide', function () {
+		var done = this.async();
+		styleguide.startServer().then(function (instance) {
+			instance.on('close', done);
+		});
+	});
 }
 ```
 
--- index.js
+++ index.js
@@ -3,27 +3,39 @@ var app = require('./app');
 var http = require('http');
 var fs = require('fs');
 var exports = module.exports = {};
+var tcpPortUsed = require('tcp-port-used');
+var colors = require('colors/safe');
 
-exports.startServer = function(options) {
-  var defaultOptions = {
-    styleguidePath: 'styleguide'
-  };
+exports.startServer = function (options) {
+	var defaultOptions = {
+		styleguidePath: 'styleguide'
+	};
 
-  options = _.assign(defaultOptions, options);
+	options = _.assign(defaultOptions, options);
 
-  var config = JSON.parse(fs.readFileSync('./' + options.styleguidePath + '/config.txt', 'utf8'));
-  var serverInstance;
-  
-  app.set('styleguideConfig', config);
-  app.set('port', config.serverPort);
-  serverInstance = http.createServer(app);
-  serverInstance.listen(config.serverPort, function() {
-    console.log('Styleguide server listening on port ' + config.serverPort);
-  }).on('error', function(error) {
-    if (error.code === 'EADDRINUSE') {
-      console.error('Port:' + config.serverPort + ' is already in use.');
-      console.error('Please provide another port.');
-    }
-  });
-  return serverInstance;
+	var config = JSON.parse(fs.readFileSync('./' + options.styleguidePath + '/config.txt', 'utf8'));
+	var serverInstance;
+
+
+	return tcpPortUsed.check(config.serverPort)
+		.then(function (inUse) {
+			if (!inUse) {
+				app.set('styleguideConfig', config);
+				app.set('port', config.serverPort);
+				serverInstance = http.createServer(app);
+				serverInstance.listen(config.serverPort, function () {
+					console.log(colors.green('Styleguide server listening on port ' + config.serverPort));
+				}).on('error', function (error) {
+					if (error.code === 'EADDRINUSE') {
+						console.error(colors.red('Something went wrong and server could not start'));
+					}
+				});
+			} else {
+				console.error(colors.red('Port:' + config.serverPort + ' is already in use.'));
+				console.error(colors.red('Please provide another port.'));
+			}
+
+			return serverInstance;
+
+		});
 };
